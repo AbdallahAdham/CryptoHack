d3edce74ec61865dac0026b69acc9b21653837a8
Abdallah Adham <abdallahadham1998@gmail.com>
2022-08-03 02:55:29 +0200

    Create ASCII.py

---
+++ ASCII/ASCII.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+
+ascii_arr = [99, 114, 121, 112, 116, 111, 123, 65, 83, 67, 73, 73, 95, 112, 114, 49, 110, 116, 52, 98, 108, 51, 125]
+
+flag = ""
+
+for i in range(len(ascii_arr)):
+    flag += chr(ascii_arr[i])
+
+print(flag)
