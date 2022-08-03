c991cc8ad7a29fa427ae25ad013ca6e394b16697
Abdallah Adham <abdallahadham1998@gmail.com>
2022-08-03 03:57:43 +0200

    Updated

--- ASCII/ASCII.py
+++ ASCII/ASCII.py
@@ -1,5 +1,10 @@
 #!/usr/bin/env python3
 
+"""
+HINTS:
+- In Python, the chr() function can be used to convert an ASCII ordinal number to a character.
+- The ord() function does the opposite.
+"""
 ascii_arr = [99, 114, 121, 112, 116, 111, 123, 65, 83, 67, 73, 73, 95, 112, 114, 49, 110, 116, 52, 98, 108, 51, 125]
 
 flag = ""
