965113a9a3bd4516222a8253f726f6cbdc193e62
Abdallah Adham <abdallahadham1998@gmail.com>
2022-08-04 23:06:52 +0200

    Update you_either_know_xor_you_don't.py

--- You either know, XOR you don't/you_either_know_xor_you_don't.py
+++ You either know, XOR you don't/you_either_know_xor_you_don't.py
@@ -1,2 +1,28 @@
 #!/usr/bin/env python3
 
+"""
+HINTS:
+- Remember the flag format and how it might help you in this challenge!
+"""
+from pwn import *
+
+hex_string = "0e0b213f26041e480b26217f27342e175d0e070a3c5b103e2526217f27342e175d0e077e263451150104"
+
+bytes_string = bytes.fromhex(hex_string)
+
+
+flag_format = 'crypto{'
+
+key = ""
+for i in range(len(flag_format)):
+    key += ''.join(chr(bytes_string[i] ^ ord(flag_format[i])))
+
+key = "myXORkey"
+
+flag = xor(bytes_string, key.encode()).decode("utf-8")
+
+print(flag)
+
+
+
+
